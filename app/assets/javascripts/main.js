daa1d7c0c7a6040fe8f2da726ea81653ef1144d8
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-10 15:03:21 -0700

    add underscore template to display quiz names

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,28 @@
+// $(document).ready(function(){
+//   $.get('/quizzes', function(data) {
+//     $('.intro').empty();
+//     _.each(data, function(element){
+//       $('body').append("<h1>"+element.title+"</h1>");
+//     });
+//   });
+// });
+
+
+
 $(document).ready(function(){
-  $.get('/quizzes', function(data) {
-    $('.intro').empty();
-    _.each(data, function(element){
-      $('body').append("<h1>"+element.title+"</h1>");
+  $.get('/quizzes', function(data){
+
+    var template = $(".my-template").html();
+    var uncompiledTemplate = _.template(template);
+    var compiledTemplate = uncompiledTemplate({
+      content: data
     });
+    var $el = $(compiledTemplate);
+    $('.intro').empty();
+    $('body').append($el);
   });
 });
+
+$(document).on("click", ".quiz-title", function(){
+  console.log("HEY BUDDY!")
+})
