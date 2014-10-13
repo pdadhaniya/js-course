c6e2f43211f6b366a6a0a719e449b29cb2c76e28
Parag Dadhaniya <paragdadhaniya@gmail.com>
2014-10-12 17:07:43 -0700

    refactor code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,12 +1,9 @@
 var counter = 0;
-var answer;
-var correct;
+var answer, correct, quizId;
 var numberCorrect = 0;
-var quizId;
 
 $(document).ready(function(){
   $.get('/quizzes', function(data){
-
     var template = $(".all-quiz-template").html();
     var uncompiledTemplate = _.template(template);
     var compiledTemplate = uncompiledTemplate({
@@ -37,23 +34,18 @@ $(document).on("click", ".quiz-choice", function(){
   var choice = $(this).attr('id');
   if (choice === answer) {
     numberCorrect += 1;
-  }
-  if (choice === answer) {
     $('.display').html("<h1>You are correct!</h1>")
   } else {
     $('.display').html("<h1>You are wrong!</h1>")
   };
-  console.log(numberCorrect);
   counter += 1;
   setTimeout(nextQuestion, 1000);
 });
 
 var nextQuestion = function(){
   $.get("/quizzes/"+quizId+"/questions", function(data){
-    console.log(data.length)
     if (data.length === counter) {
       $('.display').html("<h1>You got "+(numberCorrect*100)/data.length+"% right!</h1>");
-      console.log(numberCorrect); 
     } else {
       answer = data[counter].answer
       var templateQuiz = $(".quiz-template").html();
